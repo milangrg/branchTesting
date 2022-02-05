1bf83411c68d2431f11d4a8ea4349d3fcfbcc4d5
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2022-02-05 11:53:09 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/myamplifyapp/MainActivity.kt
+++ app/src/main/java/com/example/myamplifyapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.myamplifyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// hello world
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
